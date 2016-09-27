6db8156210520a7449609afbec9b0241b6fc9df7
Abraham Williams <4braham@gmail.com>
2016-09-27 10:32:40 -0500

    added build gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,33 @@
 
 const gulp = require('gulp');
 const serve = require('gulp-serve');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
+const polyfiller = require('gulp-polyfiller');
 
-gulp.task('serve', serve('src'));
+gulp.task('serve', ['build'], serve('build'));
+
+gulp.task('build', ['copy', 'js']);
+
+gulp.task('clean', () => {
+  return gulp.src('build/*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('js', ['polyfills'], () => {
+  return gulp.src(['src/js/*.js', '!src/js/*.min.js'])
+    .pipe(babel())
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('polyfills', () => {
+  return polyfiller.bundle(['Promise', 'Fetch'])
+    .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('copy', () => {
+  return gulp.src(['src/**/*', '!src/js/app.js'])
+    .pipe(gulp.dest('build'));
+});
